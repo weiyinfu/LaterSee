b631013394c0d21b21364e0adb32d5c1a9b713aa
weiyinfu <weiyinfu.weiyinfu@bytedance.com>
2021-06-04 00:20:12 +0800

    添加tab事件

--- haha.js
+++ haha.js
@@ -5,16 +5,28 @@ function onMenuCreated() {
 function saveAndClose(tabList) {
     //发送标签页到服务器并关闭标签页
     console.log(tabList)
-    axios.post(getUrl(), tabList).then(resp => {
+    axios.post(getUrl(), {type: "laterSee", tabList}).then(resp => {
         if (resp.data !== "ok" && resp !== "ok") {
             throw new Error("发送数据错误");
         }
         chrome.tabs.remove(tabList.map(tab => tab.id))
-    }).catch(err => {
-        setError(err)
-        window.open("error.html")
-        console.log(err)
-    })
+    }).catch(onError)
+}
+
+function onError(err) {
+    setError(err)
+    window.open("error.html")
+    console.log(err)
+}
+
+function emitEvent(kwargs) {
+    console.log(kwargs)
+    axios.post(getUrl(), kwargs).then(resp => {
+        if (resp.data !== "ok" && resp !== "ok") {
+            throw new Error("发送数据错误");
+        }
+        console.log("ok")
+    }).catch(onError)
 }
 
 const cmdList = [
@@ -75,6 +87,7 @@ const cmdList = [
 ]
 
 function main() {
+    //设置上下文菜单
     chrome.contextMenus.removeAll(function () {
         const handlerMap = {}
         for (let cmd of cmdList) {
@@ -85,6 +98,7 @@ function main() {
             const info = {
                 id: cmd.name,
                 title: cmd.name,
+                contexts: ["all"],
             }
             chrome.contextMenus.create(info, onMenuCreated);
         }
@@ -92,6 +106,27 @@ function main() {
         chrome.contextMenus.onClicked.addListener(function (info, tab) {
             handlerMap [info.menuItemId].handle();
         });
+    })
+    chrome.tabs.onCreated.addListener(tab => {
+        emitEvent({type: "create", tab})
+    })
+    chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
+        emitEvent({type: "update", tabId, info, tab})
+    })
+    chrome.tabs.onRemoved.addListener((tabId, info) => {
+        emitEvent({type: "remove", tabId, info})
+    })
+    chrome.tabs.onAttached.addListener(function (tabId, info) {
+        emitEvent({type: "attach", tabId, info})
+    });
+    chrome.tabs.onDetached.addListener((tabId, info) => {
+        emitEvent({type: "detach", tabId, info})
+    })
+    chrome.tabs.onMoved.addListener((tabId, info) => {
+        emitEvent({type: "move", tabId, info})
+    })
+    chrome.tabs.onSelectionChanged.addListener((tabId, info) => {
+        emitEvent({type: "select", tabId, info})
     })
 }
 
